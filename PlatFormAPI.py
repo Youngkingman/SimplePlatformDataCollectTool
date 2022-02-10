1765a2afa4f25bbde4442fd4324fa3786db909ba
ChenZhenyin <376407789@qq.com>
2022-02-10 15:35:21 +0800

    合并ytb和tiktok的渠道的重定向接口

--- PlatFormAPI.py
+++ PlatFormAPI.py
@@ -1,4 +1,3 @@
-from platform import platform
 import yapi
 import requests
 import re
@@ -11,7 +10,7 @@ class PlatFormAPI(object):
         self.googleAuth = googleAuth
         self.try_count = try_count
         self.time_out = time_out
-        self.headers = headers = {
+        self.headers = {
             'User-Agent' : 'Mozilla / 5.0(Windows NT 10.0; Win64; x64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 96.0.4664.45 Safari / 537.36'
         }
         self.driveConfig = {
@@ -20,10 +19,11 @@ class PlatFormAPI(object):
         }
         #正则表达式相关初始化
         self.redirec_pattern = re.compile(r'\"(.*)\"')
-        self.titok_pattern = re.compile(r'\.(tiktok)\.')
-        self.ytb_pattern = re.compile(r'\.(youtube)\.')
-        self.titok_video_pattern = re.compile(r'\/video\/(\d+)\/?\?')
-
+        self.titok_pattern = re.compile(r'(tiktok)')
+        self.ytb_pattern = re.compile(r'(youtu\.?be)')
+        self.tiktok_video_pattern = re.compile(r'\/video\/(\d+)\/?\?')
+        self.ytb_video_pattern = re.compile(r'\=(.*)\&')
+        self.ytb_video_pattern2 = re.compile(r'\=(.*)')
     # 开源tiktok api获取视频信息, 返回一个json
     def getTiktokVideoAllInfo(self, VideoId):
         api = TikTokAPI(self.cookies)
@@ -58,36 +58,28 @@ class PlatFormAPI(object):
             return
         return int(LikeCount), int(PlayCount)
 
-    # 获取重定向的url
-    def getRedirectUrl(self, url):
-        try_count = 1
-        while try_count < self.try_count:
-            if try_count > 1:
-                print("正在进行第", try_count, "次尝试")
-            try:
-                response = requests.get(url, headers=self.headers, timeout=self.time_out)
-                redirect_url = response.url
-                response.close()
-                return 0, redirect_url
-            except:
-                print("第", try_count, "次尝试失败")
-                try_count = try_count + 1
-        return -1, url
-
-    #处理tiktok链接的VideoId提取
-    def getTiktokVideoId(self,url):
-        videoId = self.titok_video_pattern.findall(url)
-        if videoId:
-            return videoId[0]
+    # videoId提取
+    def getVideoId(self, url, PlatForm):
+        video_pattern = self.ytb_video_pattern if PlatForm == "youtube" else self.tiktok_video_pattern
+        allowRedirects = True if PlatForm == "youtube" else False
+        VideoId = video_pattern.findall(url)
+        if VideoId:
+            return VideoId[0], url
         else:
-            response = requests.get(url, headers=self.headers, timeout=self.time_out, allow_redirects=False)
-            redirect_url = self.redirec_pattern.findall(response.text)[0]
-            videoId = self.titok_video_pattern.findall(redirect_url)
-            response.close()
-            if videoId:
-                return videoId[0]
-
-        return ""
+            try:
+                response = requests.get(url, headers=self.headers, timeout=self.time_out, allow_redirects=allowRedirects)
+                redirect_url = self.redirec_pattern.findall(response.text)[0] if PlatForm == "tiktok" else response.url
+                VideoId = video_pattern.findall(redirect_url)
+                response.close()
+                if VideoId:
+                    return VideoId[0], redirect_url
+                elif PlatForm == "youtube":
+                    VideoId = self.ytb_video_pattern2.findall(redirect_url)
+                    if VideoId:
+                        return VideoId[0], redirect_url
+            except:
+                return "", url
+        return "", url
 
     # 从url解析视频数据，返回平台类型和视频ID
     def urlToPlatformAndVedioId(self, url):
@@ -96,40 +88,26 @@ class PlatFormAPI(object):
         if url == "":
             return -1, PlatForm, VideoId, url
 
-        redirect_url = url
-
-        #只有两个平台先这么写
         if self.titok_pattern.search(url):
             PlatForm = 'tiktok'
         elif self.ytb_pattern.search(url):
             PlatForm = 'youtube'
         else:
-            return -3, PlatForm, VideoId, redirect_url
+            return -3, PlatForm, VideoId, url
 
-        # # 筛掉不合法链接
-        # if code == -1:
-        #     return -2, PlatForm, VideoId, url
-        # elif len(result.netloc.split('.')) < 2:
-        #     return -3, PlatForm, VideoId, redirect_url
+        try_count = 1
+        while try_count < self.try_count:
+            if try_count > 1:
+                print("正在进行第", try_count, "次尝试")
 
-
-        # 根据平台截取重定向后的链接ID
-        if PlatForm == 'youtube':
+            # 根据平台做对应的处理
             try:
-                code, redirect_url = self.getRedirectUrl(url)
-                if code == -1:
-                    return -2, PlatForm, VideoId, url
-                result = urlparse(redirect_url)
-                #建议这里也整成正则
-                VideoId = result.query.split('=')[1].split('&')[0]
+                VideoId, redirect_url = self.getVideoId(url, PlatForm)
+                if VideoId != "":
+                    return 0, PlatForm, VideoId, redirect_url
+                else:
+                    try_count = try_count + 1
             except:
-                return -4, PlatForm, VideoId, redirect_url
-        if PlatForm == 'tiktok':
-            try:
-                VideoId = self.getTiktokVideoId(url)
-                if VideoId == "":
-                    return -4, PlatForm, VideoId, redirect_url
-            except:
-                return -4, PlatForm, VideoId, redirect_url
-
-        return 0, PlatForm, VideoId, redirect_url
+                print("第", try_count, "次尝试失败")
+                try_count = try_count + 1
+        return -4, PlatForm, VideoId, url
